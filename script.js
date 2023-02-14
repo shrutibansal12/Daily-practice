e5b264b0aaea67bcf5046637efab42f7e15e6f66
shruti bansal <shruti1212bansal@gmail.com>
2023-02-14 14:27:53 +0530

    Day1

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const close = document.querySelector('.close-modal');
+const show = document.querySelectorAll('.show-modal');
+
+console.log(show);
